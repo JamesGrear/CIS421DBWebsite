6be01a136f9d4b993f6d3bda9dccfd189d1cfe92
JamesGrear <jgrear@umich.edu>
2018-04-06 22:58:28 -0400

    Login Addition
    Added the login page to the set of pages as a standalone.

--- CIS421Final/Pages/Login.php
+++ CIS421Final/Pages/Login.php
@@ -7,11 +7,27 @@
         <link rel="shortcut icon" href="../over.png">
         <link rel = "stylesheet"  href="../CSS/Styles.css"> 
     </head>
-    <body>
-        
+    <body id="login">
+     <div class="row">
+        <div class ="column" style="background-color: #f99e1a;">
+            <h2>New user? Sign up!</h2>
+            <form>
+                <label for="username">Username:</label> <input type="text" id="newuse" name="username">
+                <br>
+                <label for="password">Password:</label> <input type="text" id="newpas" name="password">
+            </form>
+        </div>
+        <div class ="column2" style="background-color: #f99e1a;">
+            <h2>Already signed up? Log in here.</h2>
+            <form>
+                <label for="username" id="old">Username:</label> <input type="text" id="use" name="username" size ="35">
+                <br>
+                <label for="password" id="old2">Password:</label> <input type="password" id="pas" name="password" size="35">
+            </form>
+        </div>
 <?php
 
 ?>
+     </div>  
   </body>
-    <footer align="center">&copy; James Grear 2018</footer>
 </html>
